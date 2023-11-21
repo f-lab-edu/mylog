d36d834e09a55a8124410cbd5eb8e89c74cb23ed
syg0629 <syg0629@hanmail.net>
2023-11-21 15:39:40 +0900

    refactor: 변수명 수정

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -6,13 +6,13 @@ import { supabase } from "@/lib/supabase/initSupabase";
 
 type Blog = Database["public"]["Tables"]["blog"]["Row"];
 
-const BlogLists = () => {
-  const [blogLists, setBlogLists] = useState<Blog[]>([]);
+const Blogs = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data: blogLists, error } = await supabase
+        const { data: blogs, error } = await supabase
           .from("blog")
           .select("*")
           .order("id", { ascending: false });
@@ -21,8 +21,8 @@ const BlogLists = () => {
           throw error;
         }
 
-        setBlogLists(blogLists);
-        console.log("Supabase 연결 성공:", blogLists);
+        setBlogs(blogs);
+        console.log("Supabase 연결 성공:", blogs);
       } catch (error) {
         if (error instanceof Error)
           console.log("Supabase 데이터 가져오는 중 오류 >> ", error.message);
@@ -34,10 +34,10 @@ const BlogLists = () => {
   return (
     <div>
       <h2>블로그 리스트</h2>
-      <h4>{blogLists.length}개의 글이 있습니다.</h4>
+      <h4>{blogs.length}개의 글이 있습니다.</h4>
       <Line />
       <div>
-        {blogLists.map((posts: Blog) => (
+        {blogs.map((posts: Blog) => (
           <BlogItem key={posts.id} {...posts} />
         ))}
       </div>
@@ -45,6 +45,5 @@ const BlogLists = () => {
   );
 };
 
-BlogLists.displayName = "BlogLists";
-
-export default memo(BlogLists);
+Blogs.displayName = "blogs";
+export default memo(Blogs);
